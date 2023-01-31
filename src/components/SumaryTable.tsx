50d3d32cf6a2096c7ffb30f27c3de384ca6d58bd
Vitor Ribeiro <vitor.camposrdc@gmail.com>
2023-01-30 21:28:50 -0300

    first commit

---
+++ src/components/SumaryTable.tsx
@@ -0,0 +1,54 @@
+import { useEffect } from "react"
+import { api } from "../lib/axios"
+import { generatesDatesFromYearBeggining } from "../utils/generate-dates-from-year-beggining"
+import { HabitDay } from "./HabitDay"
+
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+
+const sumaryDates = generatesDatesFromYearBeggining()
+
+const minimumSumaryDatesSize = 18 * 7 
+const amountOfDaysToFill = minimumSumaryDatesSize - sumaryDates.length
+
+
+export function SumaryTable() {
+
+  useEffect(() => {
+    api.get('sumary').then(response => {
+      console.log(response.data)
+    })
+  },[])
+
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {
+          weekDays.map((weekDay, i) => {
+            return (
+              <div key={`${weekDay}-${i}`} className="text-zinc-400 text-xl font-bold h-10 w-10 flex items-center justify-center">
+                 {weekDay}
+              </div>
+            )
+          })
+        }
+      </div>
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {
+          sumaryDates.map((date) => {
+            return (
+              <HabitDay 
+                key={date.toString()} 
+                amount={5}
+                completed={Math.round(Math.random() * 5)} 
+              />
+            )
+          })
+        }
+        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+          return (
+            <div key={i} className=" w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed "/>          )
+        })}
+      </div>
+    </div>
+  )
+}
